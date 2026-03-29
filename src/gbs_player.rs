efd52f111e4ad17a1dc7c50644d66a7869d2fb8a
aikiriao <samuraiaiki@gmail.com>
2026-03-29 18:20:41 +0900

    rename / add assertion.

--- src/gbs_player.rs
+++ src/gbs_player.rs
@@ -3,7 +3,7 @@ use crate::sm83::*;
 use crate::types::*;
 
 pub struct GBSPlayer {
-    sm83: SM83,
+    cpu: SM83,
     gbs_header: GBSFileHeader,
 }
 
@@ -11,7 +11,7 @@ impl GBSPlayer {
     /// コンストラクタ
     pub fn new(gbs_header: &GBSFileHeader) -> Self {
         Self {
-            sm83: SM83::new(),
+            cpu: SM83::new(),
             gbs_header: gbs_header.clone(),
         }
     }
@@ -23,33 +23,34 @@ impl GBSPlayer {
 
         assert!(load_start < 0x8000);
 
-        self.sm83.mem[load_start..load_end].copy_from_slice(data);
+        self.cpu.mem[load_start..load_end].copy_from_slice(data);
     }
 
     /// 初期化
     pub fn init(&mut self, song_number: u8) {
         // レジスタ・フラグ・RAMのクリア
-        self.sm83.reset_registers();
-        self.sm83.reset_flags();
-        self.sm83.clear_ram();
+        self.cpu.reset_registers();
+        self.cpu.reset_flags();
+        self.cpu.clear_ram();
 
         // アキュムレータに曲番号
+        assert!(song_number >= (self.gbs_header.first_song - 1));
         assert!(song_number < self.gbs_header.num_songs);
-        self.sm83.regs.a = song_number;
+        self.cpu.regs.a = song_number;
         // スタックポインタ初期化
-        self.sm83.regs.sp = self.gbs_header.stack_pointer;
+        self.cpu.regs.sp = self.gbs_header.stack_pointer;
         // PCを初期化
-        self.sm83.regs.pc = self.gbs_header.init_address;
+        self.cpu.regs.pc = self.gbs_header.init_address;
 
         // タイマー初期化
-        self.sm83
+        self.cpu
             .write_ram_u8(HWREG_TMA_TIMER_MODULO, self.gbs_header.timer_modulo);
-        self.sm83
+        self.cpu
             .write_ram_u8(HWREG_TAC_TIMER_CONTROL, self.gbs_header.timer_control);
 
         // RETが実行されるまで実行
         loop {
-            let (opcode, _) = self.sm83.execute_step();
+            let (opcode, _) = self.cpu.execute_step();
             match opcode {
                 SM83Opcode::RETNooprand => break,
                 // TODO: もしかしたら条件付きRETがくるかも...
@@ -60,11 +61,11 @@ impl GBSPlayer {
 
     /// 再生
     pub fn play(&mut self) {
-        self.sm83.regs.pc = self.gbs_header.play_address;
+        self.cpu.regs.pc = self.gbs_header.play_address;
 
         // RETが実行されるまで実行 + 定期音声出力
         loop {
-            let (opcode, _) = self.sm83.execute_step();
+            let (opcode, _) = self.cpu.execute_step();
             match opcode {
                 SM83Opcode::RETNooprand => break,
                 // TODO: もしかしたら条件付きRETがくるかも...
